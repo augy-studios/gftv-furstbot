63c59646d73be13fee5d2f2f54b636143c77d96c
Augy <ateoks@gmail.com>
2025-02-09 02:43:41 +0800

    Add environment variables for Google Sheets integration

--- telegram-bot/bot/config.py
+++ telegram-bot/bot/config.py
@@ -9,3 +9,7 @@ API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID")) if os.getenv("LOG_CHANNEL_ID") else None
 BOT_OWNER_ID = int(os.getenv("BOT_OWNER_ID")) if os.getenv("BOT_OWNER_ID") else None
+
+SHEET_URL = str(os.getenv("SHEET_URL")) if os.getenv("SHEET_URL") else None
+SHEET_HELLO = str(os.getenv("SHEET_HELLO")) if os.getenv("SHEET_HELLO") else None
+SHEET_2025 = str(os.getenv("SHEET_2025")) if os.getenv("SHEET_2025") else None
